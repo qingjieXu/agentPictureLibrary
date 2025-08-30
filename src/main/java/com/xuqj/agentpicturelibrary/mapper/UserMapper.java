faad6510e23672333d6bf96d936fac08237532f0
11394 <qingjiexu1@gmail.com>
2025-08-30 11:19:16 +0800

    用户模块，注册，登录，退出，增删改查，数据精度修正，aop权限校验

---
+++ src/main/java/com/xuqj/agentpicturelibrary/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.xuqj.agentpicturelibrary.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuqj.agentpicturelibrary.model.entity.User;
+
+/**
+* @author 11394
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-08-30 10:17:33
+* @Entity com.xuqj.agentpicturelibrary.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/com/xuqj/agentpicturelibrary/service/UserService.java
@@ -0,0 +1,98 @@
+package com.xuqj.agentpicturelibrary.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xuqj.agentpicturelibrary.model.dto.user.UserQueryRequest;
+import com.xuqj.agentpicturelibrary.model.entity.User;
+import com.xuqj.agentpicturelibrary.model.vo.LoginUserVO;
+import com.xuqj.agentpicturelibrary.model.vo.UserVO;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+
+/**
+* @author 11394
+* @description 针对表【user(用户)】的数据库操作Service
+* @createDate 2025-08-18 16:14:15
+*/
+public interface UserService extends IService<User> {
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return
+     */
+    boolean userLogout(HttpServletRequest request);
+
+    /**
+     * 查看脱敏后的用户信息
+     * @param user
+     * @return
+     */
+    UserVO getUserVO(User user);
+
+    /**
+     * 获取脱敏后的用户信息列表
+     * @param userList
+     * @return
+     */
+    List<UserVO> getUserVOList(List<User> userList);
+
+    /**
+     * 获得查询条件
+     * @param userQueryRequest
+     * @return
+     */
+    QueryWrapper<User> getQueryWrapper(UserQueryRequest userQueryRequest);
+
+    /**
+     * 获取加密后的密码
+     * @param defaultPassword
+     * @return
+     */
+    String getEncryptPassword(String defaultPassword);
+
+    /**
+     * 是否为管理员
+     *
+     * @param user
+     * @return
+     */
+    boolean isAdmin(User user);
+
+}
